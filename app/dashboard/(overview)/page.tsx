e2de6fa043d969f90875d4411bdaaeebca55ed71
0xsuid <45615666+0xsuid@users.noreply.github.com>
2024-12-12 01:00:49 +0100

    accessibility and linting

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,3 @@
-import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
@@ -9,15 +8,8 @@ import {
   LatestInvoicesSkeleton,
   CardSkeleton
 } from "@/app/ui/skeletons";
-import { fetchCardData } from "@/app/lib/data";
 
 export default async function Page() {
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
